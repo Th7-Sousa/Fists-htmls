99bc21e2e907f50aedecb098481aa4848a3ff406
th777 <th@exceptionjr.com.br>
2022-09-29 11:01:09 -0300

    att 11

--- app.js
+++ app.js
@@ -1,60 +1,41 @@
 // Qustão 1
 
-const jantar = Number(prompt("valor do jantar: "));
-const garcon = jantar * 0.1;
-const total = jantar + garcon;
-alert(`taxa do garcon R$: ${garcon.toFixed(2)} \n
-total: ${total.toFixed(3)}`);
+const novoPreco = Number(prompt("insira o novo preço:"));
+const desconto = novoPreco * 0.15;
+if (novoPreco - desconto > 500) {
+  alert("CARO");
+} else {
+  alert("Barato");
+}
 
-// Questão 2
+// Qustão 2
 
-const duracaoDias = Number(prompt("informe a quantidade de dias : "));
-const duracaoHoras = Number(prompt("informe a quantidade de horas : "));
-const totalHoras = duracaoDias * 24 + duracaoHoras;
-alert(`Duração em horas: ${totalHoras}`);
+const investimento = String(prompt("Insira o tipo de investimento: "));
+const valor = Number(prompt("inisira o valor: "));
+if (investimento == "poupança") {
+  alert(`seu rendimento será: R$ ${valor + valor * 0.5}`);
+} else if (investimento == "renda fixa") {
+  alert(`seu rendimento será: R$ ${valor + valor * 0.1}`);
+}
 
 // Qustão 3
 
-const valorConta = Number(prompt("valor da conta: "));
-const totalClientes = Number(prompt("Total de clientes: "));
-const valorPago = valorConta / totalClientes;
-alert(`Valor a ser pago por cliente é: R$ ${valorPago} reais`);
+const altura = Number(prompt("altura: "));
+const sexo = String(prompt("sexo: "));
 
-// Quetão 4
-
-const valorProduto = Number(prompt("valor de produto: "));
-const valorDesconto = valorProduto * 0.1;
-const valorTotal = valorProduto - valorDesconto;
-alert(`Valor de produto: ${valorProduto} \n
-Opções de pagamento: \n
-> cartão (em até 3 vezes ( R$ ${valorTotal / 3}) reais\n
-> boleto \n
->Á vista ( R$ ${valorTotal} reais)
-`);
-
-// Questão 5
-
-const salario = 1200;
-const vendas = Number(prompt("total de vendas:"));
-const bonus = vendas * 0.15;
-const salarioTotal = salario + bonus;
-
-alert(`seu bonus este mês é R$ ${bonus} \n
-seu salario total este mês é R$ ${salarioTotal}`);
+if (sexo == "masculino") {
+  alert(`peso ideal: ${72 * altura - 58}`);
+}
+if (sexo == "feminino") {
+  alert(`peso ideal: ${62 * altura - 44}`);
+}
 
 // Qustão 6
 
-const eleitores = 15000;
-const votosValidos = 13100;
-const votosBrancos = 1500;
-const votosNulos = 400;
+let entrada = 20;
+const numPessoas = Number(prompt("numero de pessoas: "));
+const numPeixes = Number(prompt("numero de peixes: "));
+const totalEntrada = entrada * numPessoas;
+const extra = (numPeixes - numPessoas) * 12;
 
-alert(`Total de eleitores: ${eleitores} \n
-percentual de votos válidos: ${((100 * votosValidos) / eleitores).toFixed(
-  2
-)} % \n
-percentual de votos em branco: ${((100 * votosBrancos) / eleitores).toFixed(
-  2
-)} % \n
-percentual de votos nulos: ${((100 * votosNulos) / eleitores).toFixed(2)} % \n
-`);
+alert(`valor a pagar: ${totalEntrada + extra}`);
